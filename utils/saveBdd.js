f4a9a3bcd34611253b553493cb50645dad8b8017
jokay03J <magrojoan3@gmail.com>
2022-12-31 15:50:57 +0100

    update utils
    *saveBdd: set generic function for reusable

--- utils/saveBdd.js
+++ utils/saveBdd.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 
-function Savebdd(bdd) {
-  fs.writeFile("./db/posts.json", JSON.stringify(bdd, null, 2), (err) => {
+/**
+ * @description save bdd with bdd name and bdd object
+ * @param {*} bdd
+ * @param {string} bddName
+ */
+function Savebdd(bdd, bddName) {
+  fs.writeFile(`./db/${bddName}.json`, JSON.stringify(bdd, null, 2), (err) => {
     if (err) throw err;
   });
 }
