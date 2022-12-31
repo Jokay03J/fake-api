a81e8907d8af79cc59a84753df9ea306d7413158
jokay03J <magrojoan3@gmail.com>
2022-12-31 15:50:01 +0100

    update routes
    *add todos routes and add jsdoc for posts routes and todos routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
 const { Savebdd } = require("../utils/saveBdd");
 const route = Router();
-let bdd = require("../db/posts.json");
+const bdd = require("../db/posts.json");
 const { getPostBySlug } = require("../utils/getPostBySlug");
 const { getPostsByAuthor } = require("../utils/getPostsByAuthor");
 
+/**
+ * @description get all posts(with limit and page)
+ * @default limit 30
+ * @default page 1
+ * @returns posts
+ */
 route.get("/all", (req, res) => {
   let { limit, page } = req.query;
   limit = limit ? parseInt(limit, 10) : 30;
@@ -15,10 +21,16 @@ route.get("/all", (req, res) => {
   res.json({ posts, limit: limit ? limit : 30, page: page ? page : 1 });
 });
 
+/**
+ * @description get post by slug or author
+ * @default by slug
+ * @returns posts or post
+ */
 route.get("/get", (req, res) => {
   const { query, by } = req.query;
-  if (!query)
+  if (!query) {
     return res.status(400).send({ error: true, message: "request malformed" });
+  }
 
   switch (by) {
     case "slug":
@@ -49,48 +61,68 @@ route.get("/get", (req, res) => {
   }
 });
 
+/**
+ * @description create new post
+ * @returns status 204
+ * @requires title
+ * @requires content
+ * @requires image
+ * @requires authorName
+ */
 route.post("/create", (req, res) => {
   const { title, content, image, authorName } = req.body;
-  if (!title || !content || !image || !authorName)
+  if (!title || !content || !image || !authorName) {
     return res.status(400).send({ error: true, message: "request malformed" });
+  }
 
   const slug = title.replaceAll(/[&\/\\#,+()$~%.'":*?<>{} ]/g, "-");
   if (bdd.posts.find((post) => post.slug === slug)) {
     return res.status(409).send({ error: true, message: "post already exist" });
   }
+
   bdd.posts.unshift({ title, content, image, authorName, slug });
-  Savebdd(bdd);
+  Savebdd(bdd, "posts");
   res.status(201).send({ title, content, image, authorName });
 });
 
 route.delete("/delete", (req, res) => {
   const { query } = req.query;
-  if (!query)
+  if (!query) {
     return res.status(400).send({ error: true, message: "request malformed" });
+  }
+
   const isFound = bdd.posts.find((post) => post.slug === query);
   if (!isFound) {
     return res.status(404).send({ error: true, message: "post not found" });
   }
+
   bdd.posts = bdd.posts.filter((post) => post.slug !== query);
-  Savebdd(bdd);
+  Savebdd(bdd, "posts");
   res.status(204).send();
 });
 
+/**
+ * @description modify post by slug
+ * @requires title or content or image
+ */
 route.put("/put", (req, res) => {
   const { title, content, image } = req.body;
   const { query } = req.query;
-  if (!title && !content && !image) return res.status(304).send();
+  if (!title && !content && !image) {
+    return res.status(304).send();
+  }
   const postIndex = bdd.posts.findIndex((post) => post.slug === query);
   if (postIndex === -1) {
     return res.status(404).send({ error: true, message: "post not found" });
   }
+
   bdd.posts[postIndex] = {
     ...bdd.posts[postIndex],
     title: title ? title : bdd.posts[postIndex].title,
     content: content ? content : bdd.posts[postIndex].content,
     image: image ? image : bdd.posts[postIndex].image,
   };
-  Savebdd(bdd);
+  Savebdd(bdd, "posts");
   res.json(bdd.posts[postIndex]);
 });
 
---
+++ routes/todoList.js
@@ -0,0 +1,109 @@
+const { Router } = require("express");
+const router = Router();
+const bdd = require("../db/todos.json");
+const { Savebdd } = require("../utils/saveBdd");
+
+/**
+ * @description get all todos(by limit and pages)
+ * @default limit 30
+ * @default page 1
+ */
+router.get("/all", (req, res) => {
+  let { limit, page } = req.query;
+  if (limit && page) {
+    limit = limit ? parseInt(limit, 10) : 30;
+    page = page ? parseInt(page, 10) : 1;
+    const min = (page - 1) * limit;
+    const todos = bdd.todos.slice(min, min + limit);
+
+    res.json({ todos, limit: limit ? limit : 30, page: page ? page : 1 });
+  } else {
+    res.json({ todos: bdd.todos });
+  }
+});
+
+/**
+ * @description get todo by id
+ * @requires id
+ */
+router.get("/get", (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).send({ error: true, message: "request malformed" });
+  }
+
+  const todoIndex = bdd.todos.findIndex((todo) => todo.id === id);
+  if (todoIndex === -1) {
+    return res.status(404).send({ error: true, message: "todo not found" });
+  }
+
+  res.status(200).send(bdd.todos[todoIndex]);
+});
+
+/**
+ * @description create todo
+ * @requires name
+ */
+router.post("/create", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send({ error: true, message: "request malformed" });
+  }
+
+  if (bdd.todos.find((todo) => todo.name === name)) {
+    return res.status(409).send({ error: true, message: "todo already exist" });
+  }
+
+  const { v4 } = require("uuid");
+  const id = v4();
+  bdd.todos.unshift({ name, completed: false, id });
+  Savebdd(bdd, "todos");
+  res.status(201).send({ name, completed: false, id });
+});
+
+/**
+ * @description delete todo by id
+ * @requires id
+ */
+router.delete("/delete", (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.status(400).send({ error: true, message: "request malformed" });
+  }
+
+  bdd.todos = bdd.todos.filter((todo) => todo.id !== id);
+  Savebdd(bdd, "todos");
+  res.status(204).send();
+});
+
+/**
+ * @description update todo by id
+ * @requires id
+ * @type {string}
+ * @requires completed
+ * @type {boolean}
+ */
+router.put("/put", (req, res) => {
+  const { id } = req.query;
+  const { completed } = req.body;
+
+  if (!id || typeof completed !== "boolean") {
+    return res.status(400).send({ error: true, message: "request malformed" });
+  }
+
+  const todoIndex = bdd.todos.findIndex((todo) => todo.id === id);
+  if (todoIndex === -1) {
+    return res.status(404).send({ error: true, message: "todo not found" });
+  }
+
+  bdd.todos[todoIndex] = {
+    ...bdd.todos[todoIndex],
+    completed,
+  };
+  Savebdd(bdd, "todos");
+  res.status(200).send(bdd.todos[todoIndex]);
+});
+
+module.exports = {
+  todoListRoutes: router,
+};
