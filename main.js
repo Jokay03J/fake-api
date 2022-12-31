41cdc1e6aebc29d6b83c527045ecdf91e1127e9b
jokay03J <magrojoan3@gmail.com>
2022-12-31 15:52:30 +0100

    update main script
    *add todos routes support

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config(".env");
 const { postsRoute } = require("./routes/posts");
+const { todoListRoutes } = require("./routes/todoList");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/posts", postsRoute);
+app.use("/api/todos", todoListRoutes);
 
 app.listen(PORT, () => {
   console.log(`serveur ouvert sur le port ${PORT}`);
